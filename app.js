396b1b4a6f57d28dee510824256e1386c51d96a0
Eric Russell <erictim.russell@live.com>
2022-09-01 16:44:34 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,85 @@
+const heroes = [
+  {
+    name: 'Fane',
+    damage: 7,
+    health: 100
+  },
+  {
+    name: 'Red',
+    damage: 10,
+    health: 120
+  }
+]
+
+
+
+const boss = {
+  health: 200,
+  maxHealth: 1000,
+  damage: 25,
+  level: 1
+}
+
+let goldCoins = 0
+
+function heroesDoDmg() {
+  heroes.forEach(hero => {
+    if (hero.health > 0) {
+      boss.health -= hero.damage
+    }
+    else {
+      hero.damage == 0
+    }
+  })
+  bossLevelUp()
+  drawHP()
+
+}
+
+function drawHP() {
+  // @ts-ignore
+  document.getElementById('boss Hp').innerText = `Hp:${boss.health}`
+
+
+  heroes.forEach(hero => {
+    let heroHp = document.getElementById(hero.name)
+
+    if (hero.health < 0)
+      hero.health = 0
+    // @ts-ignore
+    heroHp.querySelector('.hero').innerText = `Name: ${hero.name} Hp: ${hero.health}`
+  })
+}
+
+function bossDmg() {
+  heroes.forEach(hero => {
+    hero.health -= 25
+  })
+  drawHP()
+}
+
+function bossLevelUp() {
+  if (boss.health <= 0) {
+    boss.damage + 50
+    boss.health = 300 * boss.level
+    boss.level++
+    goldCoins += 100 * boss.level
+  }
+  drawHP()
+  drawGoldCoins()
+}
+
+function drawGoldCoins() {
+  // @ts-ignore
+  document.getElementById('coins').innerText = `GoldCoins- ${goldCoins}`
+}
+
+function healthPotion(name) {
+  let hero = heroes.find(hero => hero.health == name)
+  // @ts-ignore
+  hero.health + 100
+
+}
+
+setInterval(bossDmg, 5000)
+drawHP()
